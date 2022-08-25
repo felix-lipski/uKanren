3c1f385c440ede80511462b8e7b3fe091e6407b1
felix-lipski <felix.lipski7@gmail.com>
2022-08-25 16:21:23 +0000

    simplify

--- main.hs
+++ main.hs
@@ -3,28 +3,30 @@ import Data.Maybe
 import Data.Function
 import Control.Monad
 
-data Term a = Var Int | Atom a | Pair (Term a) (Term a) deriving (Eq, Show)
+data Term a = Var Int | Atom a | P (Term a) (Term a) deriving (Eq, Show)
 
 type Subst a = [(Int, Term a)]
 type SC a = (Int, (Subst a))
 type Goal a = SC a -> [SC a]
 
-unify :: Eq a => Subst a -> Term a -> Term a -> [Subst a]
-unify s (Atom a) (Atom b) = if a == b then [s] else []
-unify s (Var i) term = [(i, term):s]
-unify s term (Var i) = unify s (Var i) term
-unify s (Pair l1 r1) (Pair l2 r2) = bar l1 l2 s >>= bar r1 r2 where
-    bar a b s = (on (unify s) $ walk s) a b
-    walk s t@(Var i) = maybe t (walk xs) $ lookup i s where
-        xs = filter ((i/=) . fst) s
-    walk _ term = term
-unify _ _ _ = []
+walk :: Subst a -> Term a -> Term a
+walk s t@(Var i) = maybe t (walk xs) $ lookup i s where
+    xs = filter ((i/=) . fst) s
+walk _ term = term
+
+unify :: Eq a => Term a -> Term a -> Subst a -> [Subst a]
+unify t1 t2 s = case walk s <$> (t1, t2) of 
+    (Atom a, Atom b) -> if a == b then [s] else []
+    (Var i , term  ) -> [(i, term):s]
+    (term  , Var i ) -> [(i, term):s]
+    (P a c , P b d ) -> unify a b s >>= unify c d
+    _                -> []
 
 eq :: Eq a => Term a -> Term a -> Goal a
-eq a b (c, s) = map ((,) c) $ unify s a b
+eq a b (c, s) = (,) c <$> unify a b s
 
 callFresh :: (Term a -> Goal a) -> Goal a
-callFresh f (c, s) = (f (Var c)) ((c+1), s)
+callFresh f (c, s) = (f $ Var c) (c+1, s)
 
 disj :: Goal a -> Goal a -> Goal a
 disj g1 g2 = \cs -> g1 cs ++ g2 cs
@@ -32,16 +34,16 @@ disj g1 g2 = \cs -> g1 cs ++ g2 cs
 conj :: Goal a -> Goal a -> Goal a
 conj = (>=>)
 
-unify' = unify []
+unify' a b = unify a b []
 
 main = do
     print $ unify' (Var 0) (Atom "a")
-    print $ unify' (Pair (Var 3) (Var 0)) (Pair (Atom "lkj") (Atom "hjk"))
-    print $ unify' (Pair (Atom "lkj") (Atom "hjk")) (Pair (Var 0) (Var 3))
-    print $ unify' (Pair (Pair (Atom "3") (Atom "2")) (Atom "0"))
-                  (Pair (Pair (Var 0) (Var 2)) (Var 3))
-    print $ eq (Pair (Pair (Atom "a") (Var 0)) (Var 1))
-               (Pair (Pair (Var 0) (Var 1)) (Var 2)) (0, [])
+    print $ unify' (P (Var 3) (Var 0)) (P (Atom "lkj") (Atom "hjk"))
+    print $ unify' (P (Atom "lkj") (Atom "hjk")) (P (Var 0) (Var 3))
+    print $ unify' (P (P (Atom "3") (Atom "2")) (Atom "0"))
+                  (P (P (Var 0) (Var 2)) (Var 3))
+    print $ eq (P (P (Atom "a") (Var 0)) (Var 1))
+               (P (P (Var 0) (Var 1)) (Var 2)) (0, [])
     print $ callFresh (\x -> eq x (Atom "cat")) (0, [])
 
     print $ disj 
